50926ab566971bd85eb92f3a82d14d5453db3b4f
dhDSouza <dhs.danielsouza@gmail.com>
2024-08-08 01:02:50 -0300

    feat: Add aluno and curso routes on index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
 import express from 'express';
+import cursoRoutes from './routes/cursoRoutes';
+import alunoRoutes from './routes/alunoRoutes';
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
+app.use(cursoRoutes);
+app.use(alunoRoutes);
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
